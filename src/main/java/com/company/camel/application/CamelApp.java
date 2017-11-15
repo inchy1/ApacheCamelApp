898a5e89ad305eb3a0cdca2f493fa9ac36bfbec0
yaacov <yackov.inchy@gmail.com>
2017-11-15 10:32:57 +0200

    Embedding activemq in tomcat.

--- src/main/java/com/company/camel/application/CamelApp.java
+++ src/main/java/com/company/camel/application/CamelApp.java
@@ -5,10 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jms.annotation.EnableJms;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @ComponentScan(value = {"com.company.camel"})
+@EnableJms
 public class CamelApp {
 
     private static final Logger logger = LoggerFactory.getLogger(CamelApp.class);
--- src/main/java/com/company/camel/mycamel/TimerRoutingRules.java
+++ src/main/java/com/company/camel/mycamel/TimerRoutingRules.java
@@ -9,6 +9,11 @@ public class TimerRoutingRules extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("timer:foo?fixedRate=true&period=10000").routeId("myTimer")
+                .setBody(constant("Hello from Camel"))
+                .to("jms:invoices");
+        from("jms:invoices")
+                .to("file:/invoices")
                 .to("log:com.company.camel.mycamel.TimerRoutingRules?level=INFO&showException=true");
+
     }
 }
